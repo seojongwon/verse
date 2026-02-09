570cb321b7f58a0d6a8a4b408d367e2fb6851e3b
root <root@-.localdomain>
2026-02-10 00:24:23 +0900

    KOMORAN 명사 판별 서비스 추가

---
+++ komoran-service/src/main/java/com/retreatverses/komoran/KomoranServiceApplication.java
@@ -0,0 +1,11 @@
+package com.retreatverses.komoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomoranServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KomoranServiceApplication.class, args);
+    }
+}
